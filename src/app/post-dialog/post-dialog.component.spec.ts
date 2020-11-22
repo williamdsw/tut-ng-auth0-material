2a66b24cc444ca743282fd521e20f49c51fe9b78
William Santos <williamdsw@outlook.com>
2020-11-22 17:40:56 -0300

    Update Angular

--- src/app/post-dialog/post-dialog.component.spec.ts
+++ src/app/post-dialog/post-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PostDialogComponent } from './post-dialog.component';
 
@@ -6,7 +6,7 @@ describe('PostDialogComponent', () => {
   let component: PostDialogComponent;
   let fixture: ComponentFixture<PostDialogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PostDialogComponent ]
     })
